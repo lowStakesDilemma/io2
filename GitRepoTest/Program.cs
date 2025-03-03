7e4767f648aa7c76c42a61cae59ae51ca4bc4e5e
LukaszG <01180780@pw.edu.pl>
2025-03-03 09:24:55 +0100

    Initial commit

---
+++ GitRepoTest/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Globalization;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public const string NegativeNumbersExceptionMessage = "Negative numbers are not valid in this context.";
+        public const string SubstringNotIntExceptionMessage = "Substring is not an integer";
+        public static int ParseSubs(string[] subs)
+        {
+            int[] numbers = new int[subs.Length];
+            int counter = 0;
+            for (int i = 0; i < subs.Length; i++)
+            {
+                if (!int.TryParse(subs[i], out numbers[i]))
+                {
+                    throw new ArgumentException(SubstringNotIntExceptionMessage);
+                }
+                if(numbers[i] < 0)
+                {
+                    throw new ArgumentOutOfRangeException(NegativeNumbersExceptionMessage);
+                }
+                if (numbers[i] <= 1000)
+                    counter += numbers[i];
+            }
+            return counter;
+        }
+        public int Calculate(string arg)
+        {
+            List<string> delimiters = [",", "\n"];
+
+            if(arg == string.Empty) return 0;
+
+            if (arg.Length > 3 && arg[0] == '/' && arg[1] == '/') 
+            {
+                arg = arg.Substring(2);
+                int delimEndIdx = -1;
+                if (arg[0] == '[' && arg.Contains(']'))
+                {
+                    while (arg[0] == '[')
+                    {
+                        delimEndIdx = arg.IndexOf(']');
+                        string newDelim = arg.Substring(1, delimEndIdx - 1);
+                        delimiters.Add(newDelim);
+                        arg = arg.Substring(delimEndIdx + 1);
+                    }
+                }
+                else
+                {
+                    delimiters.Add(arg[0].ToString());
+                    arg = arg.Substring(1);
+                }
+            }
+
+            bool single = true;
+            foreach (string c in delimiters)
+            {
+                if (arg.Contains(c))
+                {
+                    single = false;
+                    break;
+                }
+            }
+            if (single)
+            {
+                if (int.TryParse(arg, out int result))
+                {
+                    if (result < 0)
+                    {
+                        throw new ArgumentOutOfRangeException(NegativeNumbersExceptionMessage);
+                    }
+
+                    return (result <= 1000) ? result : 0;
+                }
+                return default;
+            }
+            string[] subs = arg.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+            if (subs.Length == 2 || subs.Length == 3)
+            {
+                return ParseSubs(subs);
+            }
+            return default;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string to be calculated:");
+            string arg = "//[.][..][...]789.87...1";
+            StringCalculator calc = new();
+            Console.WriteLine($"The result is: {calc.Calculate(arg)}");
+        }
+    }
+}
---
+++ GitRepoTest/UnitTest1.cs
@@ -0,0 +1,164 @@
+using StringCalculator;
+
+
+namespace StringCalculatorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Calculate_WithEmptyString_ReturnsZero()
+        {
+            StringCalculator.StringCalculator strCalc = new StringCalculator.StringCalculator();
+
+            int expected = 0;
+            int result = strCalc.Calculate(string.Empty);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Calculate_WithSingleNumber_ReturnsValue()
+        {
+            StringCalculator.StringCalculator strCalc = new();
+
+            int[] inputs = { 1, 3, 27, 13, 3, 8 };
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                int expected = inputs[i];
+                int result = strCalc.Calculate(inputs[i].ToString());
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [TestMethod]
+        public void Calculate_TwoNumbersComma_ReturnsSum()
+        {
+            StringCalculator.StringCalculator strCalc = new StringCalculator.StringCalculator();
+
+            string[] inputs = { "22,12", "1,3", "0,  0", "100,1" };
+            int[] expectations = { 34, 4, 0, 101 };
+
+            for (int i = 0; i < expectations.Length; i++)
+            {
+                int expected = expectations[i];
+                int result = strCalc.Calculate(inputs[i]);
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [TestMethod]
+        public void Calculate_TwoNumbersNewline_ReturnsSum()
+        {
+            StringCalculator.StringCalculator stringCalc = new StringCalculator.StringCalculator();
+
+            string[] inputs = { "130\n12", "18\n20", "0\n100", "1\n1" };
+            int[] expectations = { 142, 38, 100, 2 };
+
+            for (int i = 0; i < expectations.Length; i++)
+            {
+                int expected = expectations[i];
+                int result = stringCalc.Calculate(inputs[i]);
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [TestMethod]
+        public void Calculate_ThreeNumbersNLandComma_ReturnsSum()
+        {
+            StringCalculator.StringCalculator calc = new();
+
+            string[] inputs = { "2\n1\n4", "1\n3,9", "1,4\n16", "1,5,25" };
+            int[] expectations = { 7, 13, 21, 31 };
+
+            for (int i = 0; i < expectations.Length; i++)
+            {
+                int expected = expectations[i];
+                int result = calc.Calculate(inputs[i]);
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [TestMethod]
+        public void Calculate_NegativeNumber_ThrowsException()
+        {
+            StringCalculator.StringCalculator calc = new();
+
+            string[] inputs = { "-2", "-4", "1,-12", "-2,1", "7\n-3", "-3\n7", "-1,1,1", "1,-1,1", "1,1,-1", "1,1\n-1", "1\n-1\n1" };
+            for(int i = 0;i < inputs.Length; i++)
+            {
+                try
+                {
+                    calc.Calculate(inputs[i]);
+                }
+                catch (Exception ex) 
+                {
+                    StringAssert.Contains(ex.Message, StringCalculator.StringCalculator.NegativeNumbersExceptionMessage);
+                    return;
+                }
+            }
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Calculate_BigNumbers_Ignored()
+        {
+            StringCalculator.StringCalculator calc = new();
+            string[] inputs = { "1001", "20000", "500, 1000", "500, 1001", "10000\n10000\n1", "10\n20\n2000" };
+            int[] expectations = { 0, 0, 1500, 500, 1, 30 };
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                int expected = expectations[i];
+                int result = calc.Calculate(inputs[i]);
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [TestMethod]
+        public void Calculate_CustomSingleCharDelimiters()
+        {
+            StringCalculator.StringCalculator calc = new();
+            string[] inputs = { "//#34,12", "//#34#12", "//#34\n12#1", "//#34#12#1" };
+            int[] expectations = { 46, 46, 47, 47 };
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                int expected = expectations[i];
+                int result = calc.Calculate(inputs[i]);
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [TestMethod]
+        public void Calculate_CustomMultipleCharDelimiters()
+        {
+            StringCalculator.StringCalculator calc = new();
+            string[] inputs = { "//[abrakadabra]1abrakadabra2", "//[:)]7\n8:)9", "//[:(]1,2:(3"};
+            int[] expectations = { 3, 24, 6};
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                int expected = expectations[i];
+                int result = calc.Calculate(inputs[i]);
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [TestMethod]
+        public void Calculate_MultipleCustomDelimiters()
+        {
+            StringCalculator.StringCalculator calc = new();
+            string[] inputs = { "//[.][..][...]1.2...3", "//[.][...]1...2...3", "//[..]1,2..3", "//[..]1..2\n3" };
+            int[] expectations = { 6, 6, 6, 6 };
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                int expected = expectations[i];
+                int result = calc.Calculate(inputs[i]);
+                Assert.AreEqual(expected, result);
+            }
+        }
+    }
+}
